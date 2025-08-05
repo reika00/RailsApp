24f63c24e5a7162b3671691e3c5fe1c73ad074ba
市来玲果 <ichikireika@ichikireikanoMacBook-Air.local>
2025-08-05 15:23:09 +0900

    post処理追加

---
+++ app/controllers/hello_controller.rb
@@ -0,0 +1,16 @@
+class HelloController < ApplicationController
+  protect_from_forgery
+
+  def index
+    if request.post? then
+      @title = 'Result'
+      @msg = 'you typed: ' + params['input1'] +  '.'
+      @value = params['input1']
+      else
+        @title = 'Index'
+        @msg = 'type text...'
+        @value = ''
+      end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'hello/index'
-  get 'hello', to: 'hello#index'
+  get  'hello/index',  to: 'hello#index'   # 通常アクセス
+  post 'hello/index',  to: 'hello#index'   # フォーム送信（POST）
+  get  'hello/other',  to: 'hello#other'   # 任意
 end
